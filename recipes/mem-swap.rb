9fe1866612fe13479d5501427a7d79c42224ed71
danavilacon <daniel.avilacontreras@gmail.com>
2015-04-07 09:49:09 -0300

    Some fix with recipe name change

--- recipes/mem-swap.rb
+++ recipes/mem-swap.rb
@@ -1,11 +1,11 @@
-aws_custom_metric_memory "Create memory alarm" do
+cloudwatch_memory "Create memory alarm" do
    alarm_description          "MemoryUtilization is more than 95% in 5 minutes"
    metric_threshold           "95"
    metric_comparison_operator "GreaterThanThreshold"
    metric_unit                "Percent"
 end
 
-aws_custom_metric_swap "Create swap alarm" do
+cloudwatch_swap "Create swap alarm" do
    alarm_description          "SwapUtilization is more than 20% in 5 minutes"
    metric_threshold           "20"
    metric_comparison_operator "GreaterThanThreshold"
